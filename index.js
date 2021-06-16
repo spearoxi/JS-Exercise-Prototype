3ba16b5a17187e90d15dede8f5d083edc069fca0
David Spear <davidspear@Davids-MacBook-Pro.local>
2021-06-16 19:42:11 -0400

    task 2

--- index.js
+++ index.js
@@ -90,9 +90,23 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
+
+  Car.prototype.fill = function(gallons){
+      this.tank += gallons;
+  }
+
+  const corvette = new Car('Corvette', 19);
+
+  console.log(corvette.fill(12));
+
+  corvette.fill(12);
+
   
   
   /*
